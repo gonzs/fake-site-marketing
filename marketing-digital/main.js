26bb5f2d16690ca42e7ce7a657ace0fc76cae45a
gonzs <gonzalosisner@gmail.com>
2021-11-05 20:42:19 -0300

    Add Vite + sass

--- marketing-digital/main.js
+++ marketing-digital/main.js
@@ -1,27 +1,26 @@
-"use strict"
+"use strict";
 
 window.addEventListener("load", function () {
   console.log("DOM loaded");
 
   /* Open when someone clicks on the span element */
   function openNav() {
-    document.getElementById("myNav").style.width = "100%";
+    document.getElementById("aside-nav").style.width = "100%";
   }
 
-  /* Close when someone clicks on the "x" symbol inside the overlay */
+  /* Close when someone clicks on the "x" symbol inside the nav item */
   function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("aside-nav").style.width = "0%";
   }
 
   const openMenuButton = document.getElementById("open-button");
   const closeMenuButton = document.getElementById("close-button");
-  const itemMenuButtons = document.querySelectorAll(".overlay-item");
-    
+  const itemMenuButtons = document.querySelectorAll(".nav-item");
+
   openMenuButton.addEventListener("click", openNav);
   closeMenuButton.addEventListener("click", closeNav);
-  
+
   for (let itemMenu of itemMenuButtons) {
     itemMenu.addEventListener("click", closeNav);
   }
-
 });
